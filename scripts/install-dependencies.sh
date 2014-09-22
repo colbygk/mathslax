f5bb636457ff151dfbf86dc00d170f98769b10d1
Peter Downs <peterldowns@gmail.com>
2014-09-21 21:55:54 -0400

    Install batik and use git submodules instead of re-cloning explicitly.

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -1,5 +1,15 @@
-npm install MathJax-node
-cd node_modules/MathJax-node
-git clone https://github.com/mathjax/MathJax mathjax
-cd mathjax
-git checkout develop
+pushd node_modules/MathJax-node
+git submodule init
+git submodule update
+#git clone https://github.com/mathjax/MathJax mathjax
+#pushd mathjax
+#git checkout develop
+#popd
+
+pushd batik
+curl http://apache.org/dist/xmlgraphics/batik/batik-1.7-jre13.zip > batik-1.7-jre13.zip
+jar xvf batik-1.7-jre13.zip
+mv batik-1.7/* .
+popd
+
+popd
