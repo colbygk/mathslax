c6dc8e254b92b7c873539525ebb4f6fa97c96cb7
Peter Downs <peterldowns@gmail.com>
2014-09-22 03:57:58 -0400

    Fix scripts.

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -1,15 +1,12 @@
-pushd node_modules/MathJax-node
+cd node_modules/MathJax-node
+
 git submodule init
 git submodule update
-#git clone https://github.com/mathjax/MathJax mathjax
-#pushd mathjax
-#git checkout develop
-#popd
 
-pushd batik
+cd batik
 curl http://apache.org/dist/xmlgraphics/batik/batik-1.7-jre13.zip > batik-1.7-jre13.zip
 jar xvf batik-1.7-jre13.zip
 mv batik-1.7/* .
-popd
+cd -
 
-popd
+cd -
