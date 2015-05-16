8b087ef96187ae8e2e0e5385de0d590a2ddac9f4
Svenn-Arne Dragly <s@dragly.com>
2015-05-16 18:33:59 +0200

    Corrected url to batik

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -6,7 +6,7 @@ git checkout develop
 cd ..
 
 cd batik
-curl http://apache.org/dist/xmlgraphics/batik/batik-1.7-jre13.zip > batik-1.7-jre13.zip
+curl http://apache.org/dist/xmlgraphics/batik/binaries/batik-1.7-jre13.zip > batik-1.7-jre13.zip
 jar xvf batik-1.7-jre13.zip
 mv batik-1.7/* .
 cd -
