5a1df3ddfd52602dc1087eda56f795ace672716e
Colby Gutierrez-Kraybill <colbygk@media.mit.edu>
2017-10-15 00:33:13 -0400

    More tests

--- test/unit/typeset.js
+++ test/unit/typeset.js
@@ -1,6 +1,8 @@
 
 const test = require('tap').test;
 const typeset = require('../../lib/typeset');
+const q = require('q');
+const _ = require('underscore');
 
 test('spec', function (t) {
   t.type(typeset.typeset, 'function');
@@ -10,20 +12,25 @@ test('spec', function (t) {
 
 test('createFileName', function (t) {
   const result = typeset.createFileName('ohyeah');
+  console.log(result);
+  const expected = 'static/'
+    + 'ec5f0b0bf15f3e618dcfa50f21dd666b0ec96c4f38df0c68b8418c0d32b623d8.png';
+  t.equal(result, expected);
   t.end();
 });
 
 test('extractRawMath', function (t) {
-  const result = typeset.extractRawMath('x=y','');
+  const resultOne = typeset.extractRawMath('x=y','');
+  t.type(resultOne, 'Array');
+  t.equal(resultOne.length,1);
+  const resultTwo = typeset.extractRawMath('x=y\ny=x','');
+  t.equal(resultTwo.length,1);
   t.end();
-/*
-  , function (err, res, body) {
-    t.equal(err, null);
-    t.type(res, 'object');
-    t.equal(res.statusCode, 200);
-    t.type(body, 'string');
-    t.end();
-  });
-*/
 });
 
+test('renderMath', function (t) {
+  const result = q.all(_.map(typeset.extractRawMath('x=y',''),
+    typeset.renderMath));
+  t.notEqual(result,null);
+  t.end();
+});
